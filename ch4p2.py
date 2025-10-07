5033d9c92249b8d31b6986170be4537cddf8809d
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-10-07 10:55:43 +0530

    Add files via upload

---
+++ ch4p2.py
@@ -0,0 +1,25 @@
+marks = []
+
+m1 =input("Enter maths marks :")
+marks.append(m1)
+
+m2 =input("Enter physics marks :")
+marks.append(m2)
+
+m3 =input("Enter chemistry marks :")
+marks.append(m3)
+
+m4 =input("Enter english marks :")
+marks.append(m4)
+
+m5 =input("Enter physical marks :")
+marks.append(m5)
+
+m6 =input("Enter hindi marks :")
+marks.append(m6)
+
+print(marks)
+
+marks.sort()
+
+print(marks)
