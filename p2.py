7feaf987d2d24ccc4adc03a2b0dc4e88b656b53a
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-10-10 22:49:33 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,17 @@
+p=set()
+n=input("Enter number :")
+p.add(int(n))
+
+n=input("Enter number :")
+p.add(int(n))
+
+n=input("Enter number :")
+p.add(int(n))
+
+n=input("Enter number :")
+p.add(int(n))
+
+n=input("Enter number :")
+p.add(int(n))
+
+print(p)
