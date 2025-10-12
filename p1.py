0d500c5db8641f874339b7eab4ac9851743fb04e
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-10-12 10:35:15 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,24 @@
+a=int(input("Enter first number :"))
+
+b=int(input("Enter second number :"))
+
+c=int(input("Enter third first number :"))
+
+d=int(input("Enter fourth  number :"))
+
+
+if(a>b and a>c and a>d):
+    print("a is greatest",a)
+    
+elif(b>a and  b>c and b>d):
+    print("b is greatest",b)
+
+elif(c>a and c>b and c>d):
+    print("c is greatest",c)
+
+elif(d>a and d>b and d>c):
+    print("d is greatest",d)
+
+
+
+
